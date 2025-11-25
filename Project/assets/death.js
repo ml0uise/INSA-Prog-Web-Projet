1e81ded92731323b46c98de4dca6332a4b37468c
picass02005 <clementduran0@gmail.com>
2025-11-25 17:53:50 +0100

    Update highscore / name on load

--- Project/assets/death.js
+++ Project/assets/death.js
@@ -15,11 +15,27 @@ function retry() {
     document.location.href = "../game.html";
 }
 
+function will_change_highscores() {
+    let highscores = get_highscores();
+    let min_score = highscores.slice(-1)[0].score;
+
+    if (min_score <= get_score_session()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function set_score_text() {
     const SPAN = document.querySelector("#score span");
     SPAN.textContent = String(get_score_session());
 }
 
 function on_load() {
+    if (will_change_highscores()) {
+        change_name();
+        update_highscores(get_score_session());
+    }
+
     set_score_text();
 }
