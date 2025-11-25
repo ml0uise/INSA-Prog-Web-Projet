25a3f951ac8228a9338ce9cc347ff808f03e6b4e
picass02005 <clementduran0@gmail.com>
2025-11-25 17:40:42 +0100

    Added retry button + score text

---
+++ Project/assets/death.js
@@ -0,0 +1,21 @@
+function blink_retry() {
+    const RETRY = document.querySelector("#retry");
+    
+    // Toggle visibility
+    if (RETRY.className === "hidden") {
+        RETRY.className = "";
+    } else {
+        RETRY.className = "hidden";
+    }
+}
+
+setInterval(blink_retry, 1000);
+
+function retry() {
+    document.location.href = "../game.html";
+}
+
+function set_score_text() {
+    const SPAN = document.querySelector("#score span");
+    SPAN.textContent = String(get_score_session());
+}
